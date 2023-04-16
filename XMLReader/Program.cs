6a4d639d145f530e48aa03d75742f4c1d2b53335
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-04-17 00:04:43 +1000

    Add project files.

---
+++ XMLReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+
+namespace ReadingXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("path/to/xml/file.xml");
+
+            // Select the nodes that you want to work with
+            XmlNodeList nodeList = xmlDoc.SelectNodes("//element");
+
+            // Loop through the nodes and extract data from them
+            foreach (XmlNode node in nodeList)
+            {
+                // Perform calculations on the data in the node
+                int value = int.Parse(node.Attributes["attribute"].InnerText) * 2;
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
